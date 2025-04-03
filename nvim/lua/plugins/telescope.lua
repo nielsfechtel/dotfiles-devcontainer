24be37856723c57ac8478a9c48f48e02d34aace6
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-04-03 23:59:06 +0200

    build telescope-fzf-native with make

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,77 +1,77 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-      },
-    },
-    config = function()
-      -- this was previously called with the extension-table above
-      require("telescope").setup({
-        pickers = {
-          live_grep = {
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-            additional_args = function(_)
-              return { "--hidden" }
-            end,
-          },
-          find_files = {
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-            hidden = true,
-          },
-        },
-        fzf = {
-          fuzzy = true,              -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case",  -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
-        },
-      })
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+		},
+		config = function()
+			-- this was previously called with the extension-table above
+			require("telescope").setup({
+				pickers = {
+					live_grep = {
+						file_ignore_patterns = { "node_modules", ".git", ".venv" },
+						additional_args = function(_)
+							return { "--hidden" }
+						end,
+					},
+					find_files = {
+						file_ignore_patterns = { "node_modules", ".git", ".venv" },
+						hidden = true,
+					},
+				},
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				},
+			})
 
-      local builtin = require("telescope.builtin")
+			local builtin = require("telescope.builtin")
 
-      -- find files
-      vim.keymap.set("n", "<C-p>", function()
-        builtin.find_files({ hidden = true })
-      end, { desc = "Telescope find git-tracked files only" })
-      vim.keymap.set("n", "<C-g>", function()
-        builtin.find_files({ no_ignore = true, hidden = true })
-      end, { desc = "Telescope find ALL files including gitignored" })
+			-- find files
+			vim.keymap.set("n", "<C-p>", function()
+				builtin.find_files({ hidden = true })
+			end, { desc = "Telescope find git-tracked files only" })
+			vim.keymap.set("n", "<C-g>", function()
+				builtin.find_files({ no_ignore = true, hidden = true })
+			end, { desc = "Telescope find ALL files including gitignored" })
 
-      -- search w/ text
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>fw", function()
-        local word = vim.fn.expand("<cword>")
-        builtin.grep_string({ search = word })
-      end)
-      vim.keymap.set("n", "<leader>fW", function()
-        local word = vim.fn.expand("<cWORD>")
-        builtin.grep_string({ search = word })
-      end)
+			-- search w/ text
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fw", function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end)
+			vim.keymap.set("n", "<leader>fW", function()
+				local word = vim.fn.expand("<cWORD>")
+				builtin.grep_string({ search = word })
+			end)
 
-      -- miscellaneous
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			-- miscellaneous
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
-      require("telescope").load_extension("fzf")
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			require("telescope").load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
