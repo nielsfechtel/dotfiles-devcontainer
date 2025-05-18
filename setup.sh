3ed457ddcff859251abefdb2c09496d4c7329bd7
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-05-18 18:39:47 +0200

    Update and simplify

--- setup.sh
+++ setup.sh
@@ -5,22 +5,15 @@ mkdir -p "$XDG_CONFIG_HOME"
 
 # Create symlinks for existing configurations
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/k9s" "$XDG_CONFIG_HOME"/k9s
 ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
 ln -sf "$PWD/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
 
 packages=(
-  fd
-  ripgrep
-  npm
-  lazygit
-  lazydocker
   starship
-  fzf
   tmux
-  # needed for the build of telescope-fzf-native.nvim
-  make
 )
 
 # Iterate over the array and install each package
