99aa53291ceb81b230b05290e526f57c8262755a
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-05-21 00:51:11 +0200

    Use shared files repo

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 # Create symlinks for existing configurations
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/k9s" "$XDG_CONFIG_HOME"/k9s
+ln -sf "$PWD/shared/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/shared/k9s" "$XDG_CONFIG_HOME"/k9s
 ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
-ln -sf "$PWD/tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/shared/tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/shared/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
 ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
-ln -sf "$PWD/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
 
 packages=(
   starship
